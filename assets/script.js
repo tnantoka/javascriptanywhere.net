9e9149797a5d72b54253f38d94fe95cec1a40280
tnantoka <tnantoka@bornneet.com>
2017-06-17 22:04:52 +0900

    Use CDN, Add Japanese

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,19 @@
 $(function() {
-  //$('.carousel').carousel({
-  //});
+  var $panels = $('.js-faq-panel');
+  $('.js-search').on('keyup', function() {
+    var text = $.trim($(this).val());
+
+    if (text.length) {
+      $panels.each(function() {
+        $panel = $(this);
+        if ($panel.text().indexOf(text) < 0) {
+          $panel.hide();
+        } else {
+          $panel.show();
+        }
+      });
+    } else {
+      $panels.show();
+    }
+  });
 });
