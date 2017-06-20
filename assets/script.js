499a3633d17fef7bfa5fec5d36d2da631c475715
tnantoka <tnantoka@bornneet.com>
2017-06-20 23:17:10 +0900

    Fix search faq

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,12 @@
 $(function() {
   var $panels = $('.js-faq-panel');
   $('.js-search').on('keyup', function() {
-    var text = $.trim($(this).val());
+    var text = $.trim($(this).val()).toLowerCase();
 
     if (text.length) {
       $panels.each(function() {
         $panel = $(this);
-        if ($panel.text().indexOf(text) < 0) {
+        if ($panel.text().toLowerCase().indexOf(text) < 0) {
           $panel.hide();
         } else {
           $panel.show();
